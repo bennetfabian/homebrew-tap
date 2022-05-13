1a28cd0680e68bdce9dbce397f6b6778982a34a4
Bennet Robin Fabian <bennet@bennet.dev>
2022-05-13 22:14:31 +0200

    Added Iconly Pro

---
+++ Casks/iconly-pro.rb
@@ -0,0 +1,11 @@
+cask "iconly-pro" do
+  version "0.5.0"
+  sha256 :no_check
+
+  url "https://cdn.iconly.pro/app/iconly-pro-setup-mac.dmg"
+  name "Iconly Pro"
+  desc "Iconly Pro Mac app"
+  homepage "https://iconly.pro/"
+
+  app "Iconly Pro.app"
+end
